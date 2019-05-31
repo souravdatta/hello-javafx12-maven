1ed400d22c9bf3913bc1c57e789b045b3cec7db3
Sourav Datta <sdatta@planview.com>
2019-05-31 16:14:51 +0530

    Add FXML file example

--- src/main/java/com/sdatta/tests/HelloFX.java
+++ src/main/java/com/sdatta/tests/HelloFX.java
@@ -1,6 +1,8 @@
 package com.sdatta.tests;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -9,11 +11,12 @@ import javafx.stage.Stage;
 public class HelloFX extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Test.fxml"));
+        Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
